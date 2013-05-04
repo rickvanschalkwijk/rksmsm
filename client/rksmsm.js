a3ccfbab06738d46d27f11048446fc7c621205d4
Jeroen Hoebe <hoebejeroen@gmail.com>
2013-05-05 00:13:18 +0200

    making it work
    to run do mrt run --port 3003 and to reset db use mrt reset

--- client/rksmsm.js
+++ client/rksmsm.js
@@ -2,11 +2,11 @@ console.log("rksmsm loaded");
 
 
 
-// /* user */
-// Template.login.user = function(){
-//   if(Meteor.user() != null){
-//     var username = Meteor.user().profile.name;
-//     console.log(username);
-//     return username;
-//   }
-// }
+/* user */
+Template.home.user = function(){
+  if(Meteor.user() != null){
+    var username = Meteor.user().profile.name;
+    console.log(username);
+    return username;
+  }
+}
