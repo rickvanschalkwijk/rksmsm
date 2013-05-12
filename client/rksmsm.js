f9581560f93d77c0c594b7c710de11d452ed84b6
rickvschalkwijk <rickvschalkwijk@gmail.com>
2013-05-12 16:20:53 +0200

    added js router functions

--- client/rksmsm.js
+++ client/rksmsm.js
@@ -11,6 +11,17 @@ Template.home.user = function(){
   }
 }
 
-Template.rksmsm.events({
-
+Template.home.events({
+	'click #play-alone-btn' : playAlone,
+	'click #play-group-btn' : playGroup
 });
+
+function playAlone(e, t){
+	e.preventDefault();
+	Meteor.Router.to('/singleplayer');
+}
+
+function playGroup(e, t){
+	e.preventDefault();
+	Meteor.Router.to('/multiplayer');
+}
