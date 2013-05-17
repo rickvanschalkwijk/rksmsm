5794784da42dca4c5f5ef0c77061c769abd2f259
rickvschalkwijk <rickvschalkwijk@gmail.com>
2013-05-17 18:00:54 +0200

    memory complete, only score save needs implemtation

--- client/memory.js
+++ client/memory.js
@@ -1,13 +1,19 @@
 Template.memoryGame.rendered = function(){
-	console.log("WAT");
 	$('#my-memorygame').quizyMemoryGame({
-		itemWidth: 100, 
+		itemWidth: 100,
 		itemHeight: 100, 
-		itemsMargin:40, 
-		colCount:3, 
-		animType:'flip' , 
-		flipAnim:'tb', 
-		animSpeed:250, 
-		resultIcons:true
+		itemsMargin:20,
+		colCount:3,
+		animType:'flip',
+		flipAnim:'tb',
+		animSpeed:250,
+		resultIcons:true,
+		textSummaryTitle:'Goed gedaan, je hebt het spel voltooit!',
+		textSummaryClicks: '',
+		textSummaryTime: '',
+		onFinishCall : function(param){
+									
+									setTimeout(function(){Meteor.Router.to('/singleplayer')}, 7000);
+								}
 	});
 }
