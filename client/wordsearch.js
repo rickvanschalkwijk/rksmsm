56bf6337b1dba9e92f2e67722bf7e34bdb2bc10b
rickvschalkwijk <rickvschalkwijk@gmail.com>
2013-05-27 12:56:19 +0200

    updated wordgame

--- client/wordsearch.js
+++ client/wordsearch.js
@@ -1,5 +1,5 @@
 Template.wordsearch.rendered = function(){
-	var words = "melkmeisje, nachtwacht, fietstunnel, kunst, amsterdam, vermeer, rembrandt";
+	var words = "melkmeisje, rijks, museum, kunst, vermeer";
 			
 			//attach the game to a div
 			$("#theGrid").wordsearchwidget({"wordlist" : words,"gridsize" : 10});
