d24a158c6ddfbde582f69433634ed24ab060fc4e
rickvschalkwijk <rickvschalkwijk@gmail.com>
2013-05-30 19:45:54 +0200

    high score works

--- client/highscores.js
+++ client/highscores.js
@@ -1,14 +1,27 @@
-Template.highscores.rendered = function(){
-	  Meteor.call('rankingList', Meteor.userId(), function (err, res){
-    Session.set('userHighscoreList', res);
+Template.highscores.total = function(){
+	Meteor.call('rankingList', Meteor.userId(), function (err, res){
+    Session.set('HighscoreList', res);
   });
-  var data = Session.get('userHighscoreList');
-  if(!data){
-    Meteor.call('rankingList', Meteor.userId(), function (err, res){
-      Session.set('userHighscoreList', res);
+  var data = Session.get('HighscoreList');
+
+  return data;
+}
+
+Template.highscores.SlidePuzzelBest = function(){
+	Meteor.call('rankingLevelList', Meteor.userId(), 'memory', 1, function (err, res){
+	    Session.set('userHighscoreLevelList', res);
     });
-  }
-  data = Session.get('userHighscoreList');
-  console.log(data);
+	var data = Session.get('userHighscoreLevelList');
+  return data;
+}
+
+Template.highscores.memoryBest = function(){
+  	var data = Session.get('userHighscoreLevelList');
+  	if(!data){
+    	Meteor.call('rankingLevelList', Meteor.userId(), 'memory', 1, function (err, res){
+      	Session.set('userHighscoreLevelList', res);
+    	});
+  	}
+  // console.log(data);
   return data;
 }
