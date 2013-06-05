7f8e79061e33ea50119ee788931a44f9af23b470
Jeroen Hoebe <hoebejeroen@gmail.com>
2013-06-05 16:01:14 +0200

    doesn't work yet
    can't upload if this gives error

--- client/highscores.js
+++ client/highscores.js
@@ -1,44 +1,44 @@
 
-Template.highscores.total = function(){
-	Meteor.call('rankingList', Meteor.userId(), function (err, res){
-    Session.set('HighscoreList', res);
-}
+// Template.highscores.total = function(){
+// 	Meteor.call('rankingList', Meteor.userId(), function (err, res){
+//     Session.set('HighscoreList', res);
+// }
 
-Template.highscores.userlist = function(){
-  Meteor.call('rankingList', Meteor.userId(), function (err, res){
-    Session.set('userHighscoreList', res);
-  });
-  var data = Session.get('HighscoreList');
+// Template.highscores.userlist = function(){
+//   Meteor.call('rankingList', Meteor.userId(), function (err, res){
+//     Session.set('userHighscoreList', res);
+//   });
+//   var data = Session.get('HighscoreList');
 
-  return data;
-}
+//   return data;
+// }
 
-Template.highscores.memoryBest = function(){
-  	Meteor.call('rankingLevelList', Meteor.userId(), 'memory', 1, function (err, res){
-	    Session.set('HighscoreMemory', res);
-    });
-	var data = Session.get('HighscoreMemory');
-  return data;
-}
+// Template.highscores.memoryBest = function(){
+//   	Meteor.call('rankingLevelList', Meteor.userId(), 'memory', 1, function (err, res){
+// 	    Session.set('HighscoreMemory', res);
+//     });
+// 	var data = Session.get('HighscoreMemory');
+//   return data;
+// }
 
-Template.highscores.SlidePuzzelBest = function(){
-	Meteor.call('rankingLevelList', Meteor.userId(), 'puzzel', 1, function (err, res){
-	    Session.set('HighscorePuzzel', res);
-    });
-    if(!Session.get('HighscorePuzzel')){
-    	Meteor.call('getRankingTopGame', 'puzzel', function(err, res){
-    		var data = res;
-    	});
-    }else{
-		var data = Session.get('HighscorePuzzel');
-  	}
-  return data;
-}
+// Template.highscores.SlidePuzzelBest = function(){
+// 	Meteor.call('rankingLevelList', Meteor.userId(), 'puzzel', 1, function (err, res){
+// 	    Session.set('HighscorePuzzel', res);
+//     });
+//     if(!Session.get('HighscorePuzzel')){
+//     	Meteor.call('getRankingTopGame', 'puzzel', function(err, res){
+//     		var data = res;
+//     	});
+//     }else{
+// 		var data = Session.get('HighscorePuzzel');
+//   	}
+//   return data;
+// }
 
-Template.highscores.triviaBest = function(){
-	Meteor.call('rankingLevelList', Meteor.userId(), 'trivia', 1, function (err, res){
-	    Session.set('HighscoreTrivia', res);
-    });
-	var data = Session.get('HighscoreTrivia');
-  return data;
-}
+// Template.highscores.triviaBest = function(){
+// 	Meteor.call('rankingLevelList', Meteor.userId(), 'trivia', 1, function (err, res){
+// 	    Session.set('HighscoreTrivia', res);
+//     });
+// 	var data = Session.get('HighscoreTrivia');
+//   return data;
+// }
